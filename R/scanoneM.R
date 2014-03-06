daca96618a812bc1adb163c7accb78c3a69d2bb6
ILYOUP KWAK <ikwak2@stat.wisc.edu>
2014-03-06 10:22:44 -0600

    scanoneM update

--- R/scanoneM.R
+++ R/scanoneM.R
@@ -1,103 +1,129 @@
-scanoneM <- function(cross, Y, tol=1e-7, n.perm=0, method=c("hk","f"), pheno.cols ) {
-
+scanoneM <- function(cross, Y, tol=1e-7, n.perm=0, method=c("hk","f", "sl", "ml"), pheno.cols ) {
+    
     if (missing(pheno.cols)) {
         pheno.cols = 1:nphe(cross)
     }
-
+    
     method <- match.arg(method)
     n.ind <- nind(cross) # n
     n.phe <- nphe(cross)
     n.chr <- nchr(cross)
     n.mar <- nmar(cross)
-
+    
     if(missing(Y)) {
         p <- nphe(cross)
         Y <- as.matrix(cross$pheno)
     } else {
         if(is.vector(Y)) { p = 1} else {p = ncol(Y)}
     }
-
+    
     if(n.perm == 0) {
-
-
-        E <- matrix(NA, n.ind, p)
-        X <- cbind(rep(1,n.ind))
-        E <- .Call(stats:::C_Cdqrls, X, Y, tol)$residuals
-        Sigma <- crossprod(E)
-
-        if( method == "hk") {
-            L0 <- determinant(Sigma)$modulus
-        } else {
-            L0 <- sum(diag(Sigma))
-        }
-
-        out <- NULL;
-        for(i in 1:n.chr) {
-            LOD = NULL;
-            for(j in 1:n.mar[i]) {
-                X <- cbind(rep(1,n.ind), cross$geno[[i]]$prob[,j,1])
-                E <- .Call(stats:::C_Cdqrls, X, Y, tol)$residuals
-                Sigma <- crossprod(E)
-
-                if( method == "hk") {
-                    L1 <- determinant(Sigma)$modulus
-                } else {
-                    L1 <- sum(diag(Sigma))
-                }
-
-#                LOD <- c(LOD, n.ind/2*log(L0/L1,10 ) )
-                LOD <- c(LOD, n.ind/2*log10(exp(1))*(L0 - L1) )
+        
+        if (method == "sl" || method == "ml") {
+            if (!("prob" %in% names(cross$geno[[1]]))) {
+                warning("First running calc.genoprob.")
+                cross <- calc.genoprob(cross)
             }
-            out <- rbind(out, cbind(rep(i,n.mar[i]), cross$geno[[i]]$map, LOD) )
-        }
-
-        outt <- data.frame( chr = out[,1], pos = out[,2], lod = out[,3])
-
-        class(outt) <- c("scanone", "data.frame")
-        return(outt)
-    } else {
-
-        lods = NULL;
-        for( rep in 1:n.perm) {
-            o <- sample(n.ind)
-            if(is.vector(Y)) { nY <- Y[o] } else { nY <- Y[o,] }
-
-
+            temp <- cross
+            temp$pheno[,1:p] <- Y
+            
+            outs <- scanoneF(temp, pheno.cols=1:p)
+            
+            return(outs)
+        } else {
+            
             E <- matrix(NA, n.ind, p)
             X <- cbind(rep(1,n.ind))
-            E <- .Call(stats:::C_Cdqrls, X, nY, tol)$residuals
+            E <- .Call(stats:::C <- Cdqrls, X, Y, tol)$residuals
             Sigma <- crossprod(E)
-
+            
             if( method == "hk") {
                 L0 <- determinant(Sigma)$modulus
             } else {
                 L0 <- sum(diag(Sigma))
             }
-
+            
             out <- NULL;
             for(i in 1:n.chr) {
                 LOD = NULL;
                 for(j in 1:n.mar[i]) {
                     X <- cbind(rep(1,n.ind), cross$geno[[i]]$prob[,j,1])
-                    E <- .Call(stats:::C_Cdqrls, X, nY, tol)$residuals
+                    E <- .Call(stats:::C <- Cdqrls, X, Y, tol)$residuals
                     Sigma <- crossprod(E)
-
+                    
                     if( method == "hk") {
                         L1 <- determinant(Sigma)$modulus
                     } else {
                         L1 <- sum(diag(Sigma))
                     }
-#                    LOD <- c(LOD, n.ind/2*log(L0/L1,10) )
+                    
+                                        #                LOD <- c(LOD, n.ind/2*log(L0/L1,10 ) )
                     LOD <- c(LOD, n.ind/2*log10(exp(1))*(L0 - L1) )
-
                 }
                 out <- rbind(out, cbind(rep(i,n.mar[i]), cross$geno[[i]]$map, LOD) )
             }
-
-            lods <- c(lods, max(out[,3]) )
+            
+            outt <- data.frame( chr = out[,1], pos = out[,2], lod = out[,3])
+            
+            class(outt) <- c("scanone", "data.frame")
+            return(outt)
         }
-        return(lods)
+    } else {
+        
+        
+        
+        if (method == "sl" || method == "ml") {
+            temp <- cross
+            temp$pheno[,1:p] <- Y
+            
+            outs <- scanoneF(temp, pheno.cols=1:p, n.perm = n.perm)
+            
+            return(outs)
+        } else {
+            
+            lods = NULL;
+            for( rep in 1:n.perm) {
+                o <- sample(n.ind)
+                if(is.vector(Y)) { nY <- Y[o] } else { nY <- Y[o,] }
+                
+                
+                E <- matrix(NA, n.ind, p)
+                X <- cbind(rep(1,n.ind))
+                E <- .Call(stats:::C <- Cdqrls, X, nY, tol)$residuals
+                Sigma <- crossprod(E)
+                
+                if( method == "hk") {
+                    L0 <- determinant(Sigma)$modulus
+                } else {
+                    L0 <- sum(diag(Sigma))
+                }
+                
+                out <- NULL;
+                for(i in 1:n.chr) {
+                    LOD = NULL;
+                    for(j in 1:n.mar[i]) {
+                        X <- cbind(rep(1,n.ind), cross$geno[[i]]$prob[,j,1])
+                        E <- .Call(stats:::C <- Cdqrls, X, nY, tol)$residuals
+                        Sigma <- crossprod(E)
+                        
+                        if( method == "hk") {
+                            L1 <- determinant(Sigma)$modulus
+                        } else {
+                            L1 <- sum(diag(Sigma))
+                        }
+                                        #                    LOD <- c(LOD, n.ind/2*log(L0/L1,10) )
+                        LOD <- c(LOD, n.ind/2*log10(exp(1))*(L0 - L1) )
+                        
+                    }
+                    out <- rbind(out, cbind(rep(i,n.mar[i]), cross$geno[[i]]$map, LOD) )
+                }
+                
+                lods <- c(lods, max(out[,3]) )
+            }
+            return(lods)
+        }
     }
 }
+
 
 
