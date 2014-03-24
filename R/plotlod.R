1e12120f2f05a8a07d284920e729a3b4c62e7676
ILYOUP KWAK <ikwak2@stat.wisc.edu>
2014-03-24 13:08:10 -0500

    plotlod function update, rm plot2lod

--- R/plotlod.R
+++ R/plotlod.R
@@ -1,7 +1,7 @@
-#' Plot signed LOD matrix.
+#' Plot signed or plain LOD image.
 #'
-#' Plot a signed version of a LOD matrix (taking -LOD when the QTL effect is
-#' negative), where (i,j) element is for phenotype i at marker j.
+#' Plot signed or plain LOD image. (taking -LOD when the QTL effect
+#' is negative), where (i,j) element is for phenotype i at marker j.
 #'
 #'
 #' @param output A matrix scaneone output using "hk" option.  ex) out <-
@@ -10,13 +10,12 @@
 #' effect.  Get this buy using geteffects() function.
 #' @param y Positions of phenotypes in image.
 #' @param gap The gap among chromosomes.
-#' @param off.end The margin between end of chromosome image and a line
-#' segment.
 #' @param ncol The number of color between red and blue
+#' @param rev transepose x and y axis
 #' @param \dots More graphical components of 'image.plot'.
 #' @return A graph of signed LOD matrix.
 #' @author Karl W Broman, Il-Youp Kwak, <email: ikwak2@@stat.wisc.edu>
-#' @seealso 'geteffects'
+#' @seealso \code{\link{geteffects}}
 #' @keywords hplot
 #' @examples
 #'
@@ -26,82 +25,90 @@
 #'
 #' out <- scanone(simspal, pheno.col=1:241 , method="hk")
 #' eff <- geteffects(simspal, pheno.cols=1:241)
-#'
-#' plotlod(out, eff, gap=15)
+#' nam <- names(output)[-(1:2)]
+#' y <- as.numeric(substr(nam, 2, nchar(nam)))/60
+#' plotlod(out, y, eff, gap=15)
+#' plotlod(out, y, gap=15, rev = T)
+#' plotlod(out, eff, gap=25)
 #'
 
-plotlod <- function(output, effects, y, gap=25, off.end=0.5, ncol=251, ...)
+plotlod <- function(output, effects, y, gap=25, ncol=251, rev=FALSE, ...)
 {
 
-  mar <- par("mar")
-  on.exit(par(mar=mar))
-  par(mar=c(3.1, 3.6, 0.6, 0.6), las=1)
+    if(missing(y)) {
+        y <- 1:(ncol(output)-2)
+    }
 
-  if(missing(y)) { # assume colnames in output are T#
-    nam <- names(output)[-(1:2)]
-    y <- as.numeric(substr(nam, 2, nchar(nam)))/60
-  }
-
-  templod <- as.matrix(output[,-(1:2)])
-  maxlod <- max(templod, na.rm=TRUE)
-  zlim <- c(0, maxlod)
-  val <- sqrt(seq(0, 1, len=ncol))
-  col <- rgb(1, rev(val), rev(val))
-  if(!missing(effects)) {
-    if(!all(dim(effects) == dim(templod)))
-      stop("dim(effects) doesn't conform to dim(output)")
-    templod[effects < 0] <- templod[effects<0] * -1
-    zlim <- c(-maxlod, maxlod)
-    ncol=(ncol-1)/2+1
+    templod <- as.matrix(output[,-(1:2)])
+    maxlod <- max(templod, na.rm=TRUE)
+    zlim <- c(0, maxlod)
     val <- sqrt(seq(0, 1, len=ncol))
-    col <- c(rgb(val, val, 1), rgb(1, rev(val), rev(val))[-1])
-  }
-
-  uchr <- unique(output[,1])
-  pos <- NULL
-  lod <- NULL
-  chr <- vector("list", length(uchr))
-  names(chr) <- uchr
-  for(i in seq(along=uchr)) {
-    temppos <- output[output[,1]==i,2]
-    temppos <- temppos - min(temppos)
-    temppos <- rowMeans(cbind(c(temppos[1]-off.end, temppos),
-                              c(temppos, max(temppos)+off.end)))
-    if(is.null(pos)) {
-      pos <- temppos
-      lod <- templod[output[,1]==i,]
+    col <- rgb(1, rev(val), rev(val))
+    if(!missing(effects)) {
+        if(!all(dim(effects) == dim(templod)))
+            stop("dim(effects) doesn't conform to dim(output)")
+        templod[effects < 0] <- templod[effects<0] * -1
+        zlim <- c(-maxlod, maxlod)
+        ncol=(ncol-1)/2+1
+        val <- sqrt(seq(0, 1, len=ncol))
+        col <- c(rgb(val, val, 1), rgb(1, rev(val), rev(val))[-1])
     }
-    else {
-      temppos <- max(pos)+gap + temppos
-      pos <- c(pos, temppos)
-      lod <- rbind(lod, rep(0, ncol(lod)), templod[output[,1]==i,])
+
+    uchr <- unique(output[,1])
+    pos <- NULL
+    lod <- NULL
+    chr <- vector("list", length(uchr))
+    names(chr) <- uchr
+    for(i in seq(along=uchr)) {
+        temppos <- output[output[,1]==i,2]
+        temppos <- temppos - min(temppos)
+#        temppos <- rowMeans(cbind(c(temppos[1]-off.end, temppos),
+#                                  c(temppos, max(temppos)+off.end)))
+        if(is.null(pos)) {
+            pos <- temppos
+            lod <- templod[output[,1]==i,]
+        }
+        else {
+            temppos <- max(pos)+gap + temppos
+            pos <- c(pos, temppos)
+            lod <- rbind(lod, rep(0, ncol(lod)), templod[output[,1]==i,])
+        }
+        chr[[i]] <- c(min(temppos), max(temppos))
+        chr[[i]] <- c(chr[[i]], mean(chr[[i]]))
     }
-    chr[[i]] <- c(min(temppos), max(temppos))
-    chr[[i]] <- c(chr[[i]], mean(chr[[i]]))
-  }
 
-  layout(cbind(1, 2), widths=c(5, 1))
-  image(pos, y, lod, xaxt="n", ylab="Time (hours)", xlab="",
-        zlim=zlim, col=col, mgp=c(2.6, 1, 0), bty="n")
-  title(xlab="Chromosome", mgp=c(2, 0, 0))
-  u <- par("usr")
-  yd <- 0.04
-  for(i in seq(along=chr)) {
-    rect(chr[[i]][1]-0.5, u[3], chr[[i]][2]+0.5, u[3]-diff(u[3:4])*yd, col="gray40", xpd=TRUE)
-    text(chr[[i]][3], u[3]-diff(u[3:4])*yd/2, uchr[i], col="white", xpd=TRUE)
-    rect(chr[[i]][1]-0.5, u[3], chr[[i]][2]+0.5, u[4], xpd=TRUE)
-  }
 
-  par(mar=c(8.1, 0.6, 4.6, 4.1))
-  lodscale <- seq(zlim[1], zlim[2], length=length(col))
-  image(0, lodscale, rbind(lodscale), xaxt="n", xlab="", yaxt="n",
-        col=col, zlim=zlim)
 
-  axis(side=4, at=pretty(lodscale), labels=abs(pretty(lodscale)))
-  u <- par("usr")
-  rect(u[1], u[3], u[2], u[4], xpd=TRUE)
-#  text(u[2]+diff(u[1:2])*4, maxlod/2, srt=90, "Cvi > Ler", xpd=TRUE)
-#  text(u[2]+diff(u[1:2])*4, -maxlod/2, srt=90, "Ler > Cvi", xpd=TRUE)
-#  text(u[2]+diff(u[1:2])*1.7, u[4]+diff(u[3:4])*0.05, "LOD", xpd=TRUE)
 
+    if(rev == FALSE) {
+
+        ## add more space between regend and plot
+        pos <- c(pos, max(pos) + gap)
+        lod <- rbind(lod, rep(0, ncol(lod)))
+
+        ## plot
+        image.plot(pos, y, lod, xaxt="n", ylab="Time (hours)", xlab="",
+              zlim=zlim, col=col, mgp=c(2.6, 1, 0), bty="n")
+        title(xlab="Chromosome", mgp=c(2, 0, 0))
+        u <- par("usr")
+        yd <- 0.04
+        for(i in seq(along=chr)) {
+            rect(chr[[i]][1]-.5, u[3], chr[[i]][2]+.5, u[3]-diff(u[3:4])*yd, col="gray40", xpd=TRUE)
+            text(chr[[i]][3], u[3]-diff(u[3:4])*yd/2, uchr[i], col="white", xpd=TRUE)
+            rect(chr[[i]][1]-.5, u[3], chr[[i]][2]+.5, u[4], xpd=TRUE)
+        }
+
+    } else {
+
+        image.plot(y, pos, t(lod), yaxt="n", xlab="Time (hours)", ylab="",
+              zlim=zlim, col=col, mgp=c(2.6, 1, 0), bty="n")
+        title(ylab="Chromosome", mgp=c(2, 0, 0))
+        u <- par("usr")
+        xd <- 0.04
+        for(i in seq(along=chr)) {
+            rect(u[1] - diff(u[1:2])*xd, chr[[i]][1]-.5 , u[1], chr[[i]][2]+.5, col="gray40", xpd=TRUE)
+            text(u[1]-diff(u[1:2])*xd/2, chr[[i]][3], uchr[i], col="white", xpd=TRUE)
+            rect(u[1], chr[[i]][1]-.5 , u[2], chr[[i]][2]+.5, xpd=TRUE)
+        }
+    }
 }
